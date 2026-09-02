0aa94b2e7ca45c7b65e4e46b3de8bc9bff0281ca
Sathana2807 <sathanaganesamoorthy@gmail.com>
2026-09-02 16:38:34 +0530

    Fix image paths

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -3,44 +3,57 @@ import db from '../db/db.json'
 
 function Posts() {
 
-    const[posts,setPosts]=useState([])
-    useEffect(()=>{
-        // fetch("http://localhost:3000/posts").
-        // then((data)=>data.json()).
-        // then((data)=> {setPosts(data)}).
-        // catch(err=>console.log(err))
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
         setPosts(db.posts)
-    },[])
-  return (
-    <div className="d-flex justify-content-center">
-        {posts.length>0 ?(
-            <div> 
-            {posts.map((post)=>(
-                <div className="my-3" key={post.id}> 
-                    <div className="d-flex">
-                        <img className="dp rounded-circle" src={post.user.profile_pic} alt="profile pic"/>
-                        <h5>{post.user.username}</h5>
-                    </div>
-                    <img className="image" src={post.image}></img>
-                    <div>
-                      <i className="bi bi-heart"></i> 
-                      <i className="bi bi-chat"></i>  
-                      <i className="bi bi-send"></i>
-                    </div>
-                    <div>
-                        <b>{post.likes} Likes</b>
-                    </div>
-                    <p>{post.caption}</p>
+    }, [])
+
+    return (
+        <div className="d-flex justify-content-center">
+            {posts.length > 0 ? (
+                <div>
+                    {posts.map((post) => (
+                        <div className="my-3" key={post.id}>
+
+                            <div className="d-flex">
+                                <img
+                                    className="dp rounded-circle"
+                                    src={import.meta.env.BASE_URL + post.user.profile_pic}
+                                    alt="profile pic"
+                                />
+
+                                <h5>{post.user.username}</h5>
+                            </div>
+
+                            <img
+                                className="image"
+                                src={import.meta.env.BASE_URL + post.image}
+                                alt="post"
+                            />
+
+                            <div>
+                                <i className="bi bi-heart"></i>
+                                <i className="bi bi-chat"></i>
+                                <i className="bi bi-send"></i>
+                            </div>
+
+                            <div>
+                                <b>{post.likes} Likes</b>
+                            </div>
+
+                            <p>{post.caption}</p>
+
+                        </div>
+                    ))}
                 </div>
-            ))}
-            </div>
-            ):(
+            ) : (
                 <div>
                     loading posts
                 </div>
             )}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Posts
