021bc7854cf4455f5c1e6d630a63de827579d9b1
Sathana2807 <sathanaganesamoorthy@gmail.com>
2026-09-02 19:57:28 +0530

    Update Profile.jsx

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,75 +1,97 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import db from '../db/db.json'
 
 function Profile() {
-    const[profile,setProfile]=useState(null)
-    const[follower,setFollower]=useState([])
-    const[unfollow,setUnfollow]=useState(0)
-    useEffect(()=>{
-        axios.get('http://localhost:3000/profile')
-        .then(data=>{setProfile(data.data);})
-        .catch(err=>console.log(err))
+  const [profile, setProfile] = useState(null)
+  const [follower, setFollower] = useState([])
 
-        axios.get('http://localhost:3000/follower')
-        .then(data=>{setFollower(data.data)})
-        .catch(err=>console.log(err))
-    },[unfollow])
+  useEffect(() => {
+    setProfile(db.profile)
+    setFollower(db.follower)
+  }, [])
 
-function HandleOnChange(e){
-    setProfile((prev)=>({
-        ...prev,
-        [e.target.name]: e.target.value
+  function HandleOnChange(e) {
+    setProfile((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value
     }))
-}
-const handleUpdata =async()=>{
-    axios.put('http://localhost:3000/profile',profile)
-    .then(console.log("Updated"))
-    .catch(err=> console.log(err))
-}
-const handleUnfollow=async(id)=>{
-    axios.delete(`http://localhost:3000/follower/${id}`)
-    .then(alert("unfollow"))
-    .then(setUnfollow(!unfollow))
-    .catch(err=>console.log(err))
-}
+  }
+
+  const handleUpdata = () => {
+    alert("Updated")
+  }
+
+  const handleUnfollow = (id) => {
+    setFollower((prev) =>
+      prev.filter((item) => item.id !== id)
+    )
+    alert("unfollow")
+  }
+
   return (
     <div className='m-3'>
-        {profile ?(
+
+      {profile ? (
         <div>
-            <img src={profile.profile_pic} className="profile rounded-circle"></img>
-          <h5>{profile.username} </h5>
-          <input type="text" 
+          <img
+            src={import.meta.env.BASE_URL + profile.profile_pic}
+            className="profile rounded-circle"
+            alt="profile"
+          />
+
+          <h5>{profile.username}</h5>
+
+          <input
+            type="text"
             name="username"
             value={profile.username}
             className="form-control my-4"
             onChange={HandleOnChange}
-        />
-        <input type='text' 
-            name="profile_pic" 
+          />
+
+          <input
+            type="text"
+            name="profile_pic"
             value={profile.profile_pic}
             className="form-control"
             onChange={HandleOnChange}
-        />
-        <button className="btn btn-primary my-4" onClick={handleUpdata}>
+          />
+
+          <button
+            className="btn btn-primary my-4"
+            onClick={handleUpdata}
+          >
             update
-        </button>
-        </div>):
-        (<div>
-            loading profile
+          </button>
         </div>
-        )}
-       {follower.length>0 ?(
-        follower.map(follower=>(
-            <div key={follower.id} className='d-flex my-2'>
-                {follower.username}
-                {<button className="btn btn-secondary ms-auto"
-                    onClick={()=>{handleUnfollow(follower.id)}}
-                >unfollow</button>}
-            </div>
+      ) : (
+        <div>
+          loading profile
+        </div>
+      )}
+
+      {follower.length > 0 ? (
+        follower.map((follower) => (
+          <div
+            key={follower.id}
+            className='d-flex my-2'
+          >
+            {follower.username}
+
+            <button
+              className="btn btn-secondary ms-auto"
+              onClick={() => handleUnfollow(follower.id)}
+            >
+              unfollow
+            </button>
+          </div>
         ))
-       ):
-       (<div>loading follower</div>)}
+      ) : (
+        <div>loading follower</div>
+      )}
+
     </div>
   )
 }
+
 export default Profile
