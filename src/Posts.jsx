6bc5b53b0389b92bc5eaaf6f30a72876a252e044
Sathana2807 <sathanaganesamoorthy@gmail.com>
2026-09-02 14:06:33 +0530

    Fix posts data loading

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import db from '../db/db.json'
 
 function Posts() {
 
     const[posts,setPosts]=useState([])
     useEffect(()=>{
-        fetch("http://localhost:3000/posts").
-        then((data)=>data.json()).
-        then((data)=> {setPosts(data)}).
-        catch(err=>console.log(err))
+        // fetch("http://localhost:3000/posts").
+        // then((data)=>data.json()).
+        // then((data)=> {setPosts(data)}).
+        // catch(err=>console.log(err))
+        setPosts(db.posts)
     },[])
   return (
     <div className="d-flex justify-content-center">
