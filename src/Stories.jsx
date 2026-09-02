b5c61adf39c4dbd03ea7b0d4561e68e572705a27
Sathana2807 <sathanaganesamoorthy@gmail.com>
2026-09-02 17:20:18 +0530

    Update Stories.jsx

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,34 +1,52 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import db from '../db/db.json'
+
 function Stories() {
-  const[Stories,setStories]=useState([])
-  const navigate=useNavigate()
-  let tot=0;
-  useEffect(()=>{
-    fetch("http://localhost:3000/story").
-    then(data=>data.json()).
-    then(data=>setStories(data)).
-    catch(err=>console.log(err))
-  },[])
-  
+  const [Stories, setStories] = useState([])
+  const navigate = useNavigate()
+  let tot = 0
+
+  useEffect(() => {
+    setStories(db.story)
+  }, [])
+
   return (
     <div className="story d-flex">
       <div className="d-none">
-        {tot=Stories.length}
+        {tot = Stories.length}
       </div>
-      {Stories.length > 0 ?(
-        Stories.map((story)=>(
-          <div key={story.id} className='mx-1' onClick={()=>{navigate(`/story/${story.id}/${tot}`)}}>
+
+      {Stories.length > 0 ? (
+        Stories.map((story) => (
+          <div
+            key={story.id}
+            className="mx-1"
+            onClick={() => {
+              navigate(`/story/${story.id}/${tot}`)
+            }}
+          >
             <div className="gradient-border">
-            <img src={story.user.profile_pic} alt='dp' className="story-dp rounded-circle"/>
+              <img
+                src={import.meta.env.BASE_URL + story.user.profile_pic}
+                alt="dp"
+                className="story-dp rounded-circle"
+              />
             </div>
-            <p className="text-truncate" style={{width:"50px"}}>{story.user.username}</p>
+
+            <p
+              className="text-truncate"
+              style={{ width: "50px" }}
+            >
+              {story.user.username}
+            </p>
           </div>
         ))
-      ):(
-      <p>
-        Loading...
-      </p>)}
+      ) : (
+        <p>
+          Loading...
+        </p>
+      )}
     </div>
   )
 }
