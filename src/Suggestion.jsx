3421098451387ff6a8d3e73f5156ed84375cbf36
Sathana2807 <sathanaganesamoorthy@gmail.com>
2026-09-03 00:02:51 +0530

    Suggestion.jsx

--- src/Suggestion.jsx
+++ src/Suggestion.jsx
@@ -65,14 +65,12 @@ function Suggestion() {
         {suggestions.length > 0 ? (
           <div>
             {suggestions.map((suggestion) => {
-
               const isFollowing = following.some(
                 (item) => item.id === suggestion.id
               )
 
               return (
                 <div className="my-1" key={suggestion.id}>
-
                   <div className="d-flex">
 
                     <img
@@ -88,128 +86,22 @@ function Suggestion() {
 
                     <button
                       className="ms-auto btn btn-link text-primary"
-                      onClick={() =>
-                        !isFollowing &&
-                        handleFollow(suggestion)
-                      }
+                      onClick={() => {
+                        if (!isFollowing) {
+                          handleFollow(suggestion)
+                        }
+                      }}
                     >
                       {isFollowing ? "Following" : "Follow"}
                     </button>
 
                   </div>
-
                 </div>
               )
             })}
           </div>
         ) : (
           <div>loading</div>
-        )}
-
-      </div>
-    </div>
-  )
-}
-
-export default Suggestion
-  const [following, setFollowing] = useState(() => {
-    const saved = localStorage.getItem("following")
-    return saved ? JSON.parse(saved) : []
-  })
-
-  const handleFollow = (id) => {
-    setFollowing((prev) => {
-      const newFollowing = [...prev, id]
-      localStorage.setItem("following", JSON.stringify(newFollowing))
-      return newFollowing
-    })
-  }
-
-  useEffect(() => {
-    const savedProfile = localStorage.getItem("profile")
-
-    if (savedProfile) {
-      setProfile(JSON.parse(savedProfile))
-    } else {
-      setProfile(db.profile)
-    }
-
-    setSuggestions(db.suggestion)
-  }, [])
-
-  return (
-    <div>
-      <div className="suggestion w-75 m-4">
-
-        {profile ? (
-          <div className="d-flex">
-
-            <img
-              className="dp rounded-circle"
-              src={
-                import.meta.env.BASE_URL +
-                profile.profile_pic
-              }
-              alt="profile pic"
-            />
-
-            <h5>{profile.username}</h5>
-
-            <small className="ms-auto text-primary">
-              Switch
-            </small>
-
-          </div>
-        ) : (
-          <p>Loading</p>
-        )}
-
-        <div className="d-flex m-2">
-          <p>Suggestion for you</p>
-          <b className="ms-auto">See all</b>
-        </div>
-
-        {suggestions.length > 0 ? (
-          <div>
-
-            {suggestions.map((suggestion) => (
-              <div
-                className="my-1"
-                key={suggestion.id}
-              >
-                <div className="d-flex">
-
-                  <img
-                    className="dp rounded-circle"
-                    src={
-                      import.meta.env.BASE_URL +
-                      suggestion.profile_pic
-                    }
-                    alt="profile pic"
-                  />
-
-                  <h5>{suggestion.username}</h5>
-
-                  <button
-                    className="ms-auto btn btn-link text-primary"
-                    onClick={() =>
-                      handleFollow(suggestion.id)
-                    }
-                  >
-                    {following.includes(suggestion.id)
-                      ? "Following"
-                      : "Follow"}
-                  </button>
-
-                </div>
-              </div>
-            ))}
-
-          </div>
-        ) : (
-          <div>
-            loading
-          </div>
         )}
 
       </div>
