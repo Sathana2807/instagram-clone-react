6913124d7bb9a3bd0ccd0becdf3a3c04e565a30c
Sathana2807 <sathanaganesamoorthy@gmail.com>
2026-09-02 17:27:36 +0530

    Fix suggestion for GitHub Pages

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -12,45 +12,34 @@ function Posts() {
     return (
         <div className="d-flex justify-content-center">
             {posts.length > 0 ? (
-                <div>
-                    {posts.map((post) => (
-                        <div className="my-3" key={post.id}>
+            <div>
+            {posts.map((post) => (
+            <div className="my-3" key={post.id}>
+            <div className="d-flex">
+            <img
+             className="dp rounded-circle"
+             src={import.meta.env.BASE_URL + post.user.profile_pic}
+             alt="profile pic"/>
+            <h5>{post.user.username}</h5>
+            </div>
+            <img className="image" src={import.meta.env.BASE_URL + post.image} alt="post"/>
 
-                            <div className="d-flex">
-                                <img
-                                    className="dp rounded-circle"
-                                    src={import.meta.env.BASE_URL + post.user.profile_pic}
-                                    alt="profile pic"
-                                />
+            <div><i className="bi bi-heart"></i>
+                <i className="bi bi-chat"></i>
+                <i className="bi bi-send"></i>
+            </div>
 
-                                <h5>{post.user.username}</h5>
-                            </div>
-
-                            <img
-                                className="image"
-                                src={import.meta.env.BASE_URL + post.image}
-                                alt="post"
-                            />
-
-                            <div>
-                                <i className="bi bi-heart"></i>
-                                <i className="bi bi-chat"></i>
-                                <i className="bi bi-send"></i>
-                            </div>
-
-                            <div>
-                                <b>{post.likes} Likes</b>
-                            </div>
-
-                            <p>{post.caption}</p>
-
-                        </div>
-                    ))}
-                </div>
+            <div>
+            <b>{post.likes} Likes</b>
+            </div>
+            <p>{post.caption}</p>
+             </div>
+            ))}
+            </div>
             ) : (
-                <div>
-                    loading posts
-                </div>
+            <div>
+            loading posts
+            </div>
             )}
         </div>
     )
