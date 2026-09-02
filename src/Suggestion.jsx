4fc8c97be9d01d682113924ea7eda1092c598bbf
Sathana2807 <sathanaganesamoorthy@gmail.com>
2026-09-02 23:18:31 +0530

    Suggestion.jsx

--- src/Suggestion.jsx
+++ src/Suggestion.jsx
@@ -4,11 +4,20 @@ import db from '../db/db.json'
 function Suggestion() {
   const [profile, setProfile] = useState(null)
   const [suggestions, setSuggestions] = useState([])
+
   const handleFollow = (id, username) => {
     alert('followed')
   }
+
   useEffect(() => {
-    setProfile(db.profile)
+    const savedProfile = localStorage.getItem("profile")
+
+    if (savedProfile) {
+      setProfile(JSON.parse(savedProfile))
+    } else {
+      setProfile(db.profile)
+    }
+
     setSuggestions(db.suggestion)
   }, [])
 
@@ -18,11 +27,22 @@ function Suggestion() {
 
         {profile ? (
           <div className="d-flex">
-            <img className="dp rounded-circle"
-              src={import.meta.env.BASE_URL + profile.profile_pic}
-              alt="profile pic"/>
+
+            <img
+              className="dp rounded-circle"
+              src={
+                import.meta.env.BASE_URL +
+                profile.profile_pic
+              }
+              alt="profile pic"
+            />
+
             <h5>{profile.username}</h5>
-            <small className="ms-auto text-primary">Switch</small>
+
+            <small className="ms-auto text-primary">
+              Switch
+            </small>
+
           </div>
         ) : (
           <p>Loading</p>
@@ -32,21 +52,40 @@ function Suggestion() {
           <p>Suggestion for you</p>
           <b className="ms-auto">See all</b>
         </div>
+
         {suggestions.length > 0 ? (
           <div>
             {suggestions.map((suggestion) => (
-              <div className="my-1" key={suggestion.id}>
+              <div
+                className="my-1"
+                key={suggestion.id}
+              >
                 <div className="d-flex">
-                  <img className="dp rounded-circle"
+
+                  <img
+                    className="dp rounded-circle"
                     src={
                       import.meta.env.BASE_URL +
                       suggestion.profile_pic
-                    }alt="profile pic"/>
+                    }
+                    alt="profile pic"
+                  />
+
                   <h5>{suggestion.username}</h5>
-                  <a className="ms-auto text-primary"
-                    onClick={() =>handleFollow(suggestion.id,suggestion.username)} >
-                    Follow</a>
-                    </div>
+
+                  <a
+                    className="ms-auto text-primary"
+                    onClick={() =>
+                      handleFollow(
+                        suggestion.id,
+                        suggestion.username
+                      )
+                    }
+                  >
+                    Follow
+                  </a>
+
+                </div>
               </div>
             ))}
           </div>
@@ -55,8 +94,10 @@ function Suggestion() {
             loading
           </div>
         )}
+
       </div>
     </div>
   )
 }
+
 export default Suggestion
