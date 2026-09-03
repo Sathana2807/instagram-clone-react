7dff0af8263185412bbb7a1d1b2b41736eeb88c7
Sathana2807 <sathanaganesamoorthy@gmail.com>
2026-09-03 11:44:51 +0530

    Posts.jsx

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -2,12 +2,26 @@ import React, { useEffect, useState } from 'react'
 import db from '../db/db.json'
 
 function Posts() {
-
     const [posts, setPosts] = useState([])
 
     useEffect(() => {
         setPosts(db.posts)
     }, [])
+
+    const handleHeartClick = (id) => {
+        setPosts(posts.map((post) =>
+            post.id === id
+                ? { ...post, liked: !post.liked }
+                : post
+        ))
+    }
+    const handleDoubleClick = (id) => {
+        setPosts(posts.map((post) =>
+            post.id === id
+                ? { ...post, liked: true }
+                : post
+        ))
+    }
 
     return (
         <div className="d-flex justify-content-center">
@@ -26,22 +40,30 @@ function Posts() {
                                 <h5>{post.user.username}</h5>
                             </div>
 
-                            <img
-                                className="image"
+                        
+                            <img className="image"
                                 src={import.meta.env.BASE_URL + post.image}
                                 alt="post"
+                                onDoubleClick={() => handleDoubleClick(post.id)}
                             />
-
                             <div>
-                                <i className="bi bi-heart"></i>
+                            <i
+                                    className={`bi ${
+                                        post.liked
+                                            ? 'bi-heart-fill text-danger'
+                                            : 'bi-heart'
+                                    }`}
+                                    onClick={() => handleHeartClick(post.id)}
+                                    style={{ cursor: 'pointer' }}></i>
+
                                 <i className="bi bi-chat"></i>
                                 <i className="bi bi-send"></i>
                             </div>
-
                             <div>
-                                <b>{post.likes} Likes</b>
+                                <b>
+                                    {post.likes + (post.liked ? 1 : 0)} Likes
+                                </b>
                             </div>
-
                             <p>{post.caption}</p>
 
                         </div>
